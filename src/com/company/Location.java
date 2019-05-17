c6168a11a353a3e3983a96226a0b51b5dabe94a4
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-05-17 16:46:46 +0300

    again update

--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -11,7 +11,7 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = exits;
+        this.exits = new HashMap<>(exits);
         this.exits.put("Q", 0);
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -53,6 +53,7 @@ public class Main {
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
             if (loc == 0) {
                 break;
             }
